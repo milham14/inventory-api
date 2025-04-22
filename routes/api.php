1af707d0f0e3a6ccb7a4dec4936b0582361f6425
milham14 <m.ilham1095@gmail.com>
2025-04-22 16:21:57 +0700

    CRUD User

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\UserController;
 
-//Route::get('/users', [UserController::class, 'index']);
-//Route::post('/users', [UserController::class, 'store']);
-//Route::put('/users/{id}', [UserController::class, 'update']);
-//Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
 // Add this route to handle fetching users
 Route::middleware('auth:sanctum')->get('/users', [UserController::class, 'index']);
@@ -24,4 +20,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/users', [UserController::class, 'index']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
 });
